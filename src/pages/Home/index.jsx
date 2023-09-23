546a749ff51d5c0c13280af89fce97a6f757929d
Faqihazh <faqihazh@gmail.com>
2023-09-23 16:38:26 +0700

    features n pages update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { TodoApp } from "../../assets/components/TodoApp";
 
 export const Home = () => {
   return (
-    <div className="bg-yellow-500 min-h-screen flex justify-center items-center ">
+    <div className="bg-amber-400 min-h-screen flex justify-center items-center ">
       <TodoApp />
     </div>
   );
