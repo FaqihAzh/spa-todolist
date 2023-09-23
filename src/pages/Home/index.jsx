560723ffc690af9ae5b516b5087ecfd4c673963c
FaqihAzh <93028236+FaqihAzh@users.noreply.github.com>
2023-09-23 16:29:59 +0700

    Update pages bg

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { TodoApp } from "../../assets/components/TodoApp";
 
 export const Home = () => {
   return (
-    <div className="bg-yellow-500 min-h-screen flex justify-center items-center ">
+    <div className="bg-[#252837] min-h-screen flex justify-center items-center ">
       <TodoApp />
     </div>
   );
